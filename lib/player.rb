d7372b2a7f7bedbdaa7e29a05eaee251b62f7bbf
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2025-06-27 10:49:15 +0200

    format validation added for the player input

--- lib/player.rb
+++ lib/player.rb
@@ -2,17 +2,34 @@ class Player
   attr_accessor :name, :color
 
   def initialize(name, color)
-    self.name = name
-    self.color = color
+    @name = name
+    @color = color
   end
 
   def select_piece
-    puts 'Select one of your pieces by entering its square (e.g. "e2"):'
-    gets.chomp
+    input = nil
+    loop do
+      puts 'Select one of your pieces by entering its square (e.g. "e2"):'
+      input = gets.chomp.downcase
+      return input if chess_square?(input)
+
+      puts 'Invalid input, please enter a square like "e2".'
+    end
   end
 
   def select_destination
-    puts 'Select a destination square (e.g. "e4"), or type "cancel" to select a different piece:'
-    gets.chomp
+    input = nil
+    loop do
+      puts 'Select a destination square (e.g. "e4"), or type "cancel" to select a different piece:'
+      input = gets.chomp.downcase
+      return input if chess_square?(input) || input == 'cancel'
+
+      puts 'Invalid input, please enter a valid square or "cancel".'
+    end
+  end
+
+  def chess_square?(string)
+    # returning a boolean indicating if the string represents a valid chess square
+    !!string.match(/^[a-h][1-8]$/)
   end
 end
