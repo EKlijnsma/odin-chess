18d4a6328ec5212be525dde39c1a37fa1032b849
Emiel Klijnsma <112609864+EKlijnsma@users.noreply.github.com>
2025-07-08 14:04:38 +0000

    test castles? method

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -43,4 +43,45 @@ describe King do
       expect(targets).to match_array(expected)
     end
   end
+
+  describe '#castles?' do
+    let(:white_king) { King.new(:white) }
+    let(:black_king) { King.new(:black) }
+
+    context 'with valid castling moves' do
+      it 'returns true for white kingside castling' do
+        expect(white_king.castles?([7, 4], [7, 6])).to be true
+      end
+
+      it 'returns true for white queenside castling' do
+        expect(white_king.castles?([7, 4], [7, 2])).to be true
+      end
+
+      it 'returns true for black kingside castling' do
+        expect(black_king.castles?([0, 4], [0, 6])).to be true
+      end
+
+      it 'returns true for black queenside castling' do
+        expect(black_king.castles?([0, 4], [0, 2])).to be true
+      end
+    end
+
+    context 'with invalid castling attempts' do
+      it 'returns false for white king trying black castling square' do
+        expect(white_king.castles?([0, 4], [0, 6])).to be false
+      end
+
+      it 'returns false for black king trying white castling square' do
+        expect(black_king.castles?([7, 4], [7, 2])).to be false
+      end
+
+      it 'returns false if from square is wrong' do
+        expect(white_king.castles?([7, 3], [7, 6])).to be false
+      end
+
+      it 'returns false if to square is wrong' do
+        expect(white_king.castles?([7, 4], [7, 5])).to be false
+      end
+    end
+  end
 end
